b96ff7cd3087d3a35b57428b24eaf1e1c8bdc163
Eugeniu Cazmal <eugeniucazmal@gmail.com>
2025-09-08 15:47:04 +0300

    Add logo.svg and update frontend to use SVG logo

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2245,7 +2245,7 @@ const Login = () => {
       <div className="login-card">
         <div className="login-header" style={{ textAlign: 'center' }}>
           <img 
-            src="/cashpot-v5/logo.png" 
+            src="/cashpot-v5/logo.svg" 
             alt="CASHPOT Logo" 
             style={{
               width: '300px',
